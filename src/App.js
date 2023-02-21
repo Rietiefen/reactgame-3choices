28ca107e0873667daaf2dfafbee54e1275279918
Rietiefen <115991935+Rietiefen@users.noreply.github.com>
2023-02-21 15:30:01 +0100

    questions made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Answer from "./components/Answer";
 
 function App() {
+  const questions = [
+    { questionText: `Which is the longest river in the world?` },
+    (answerOptions = [
+      { answerText: `Nile(Africa)`, isCorrect: true },
+      { answerText: `Amazon(SouthAmerica)`, isCorrect: false },
+      { answerText: `Yangtze(China)`, isCorrect: false },
+    ]),
+    { questionText: `Which is the world biggest continent?` },
+    (answerOptions = [
+      { answerText: `North America`, isCorrect: false },
+      { answerText: `Asia`, isCorrect: true },
+      { answerText: `Africa`, isCorrect: false },
+    ]),
+    { questionText: `Which mushrooms are poisonous?` },
+    (answerOptions = [
+      { answerText: `Amanita virosa`, isCorrect: true },
+      { answerText: `Shiitake Mushroom`, isCorrect: false },
+    ]),
+  ];
   return (
     <div className="App">
       <Answer />
