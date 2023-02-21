8b74f91e37a07964d8f9349df770f7004480f498
Rietiefen <115991935+Rietiefen@users.noreply.github.com>
2023-02-21 15:07:44 +0100

    answer.jsx added

---
+++ src/Answer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Answer = () => {
+  return (
+    <>
+      <h1>3 Choice Quiz</h1>
+      <h2>
+        <span></span>
+        <br></br>Which country has the largest population in the world?
+      </h2>
+      <ul>
+        <li>India</li>
+        <li>China</li>
+        <li>USA</li>
+      </ul>
+    </>
+  );
+};
+
+export default Answer;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import Answer from "./components/Answer";
 
 function App() {
   return (
-    <>
-      <h1>3 Choice Quiz</h1>
-      <h2>
-        <span></span>
-        <br></br>Which country has the largest population in the world?
-      </h2>
-      <ul>
-        <li>India</li>
-        <li>China</li>
-        <li>USA</li>
-      </ul>
-    </>
+    <div className="App">
+      <Answer />
+    </div>
   );
 }
 
