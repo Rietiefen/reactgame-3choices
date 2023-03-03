ead47f08917f38040a6f8e3ca4c42cc464a5706a
Rietiefen <115991935+Rietiefen@users.noreply.github.com>
2023-03-03 14:24:05 +0100

    css updated

--- src/Answer.js
+++ src/Answer.js
@@ -5,27 +5,29 @@ import Button from "react-bootstrap/Button";
 const Answer = ({ handleAnswerButtonClick, questions, currentQuestion }) => {
   return (
     <>
-      <h1>3 Choice Quiz</h1>
-      <h2>
-        <span>
-          Question&ensp;
-          {currentQuestion + 1}
-        </span>
-        <br></br>
-        {questions[currentQuestion].questionText}
-      </h2>
-      <ul id="btn">
-        {questions[currentQuestion].answerOptions.map((answerOption, key) => (
-          <li
-            key={key}
-            onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}
-          >
-            <Button className="mt-4" variant="primary">
-              {answerOption.answerText}
-            </Button>
-          </li>
-        ))}
-      </ul>
+      <div className="w3-container questions py-5">
+        <h1>3 Choice Quiz</h1>
+        <h2>
+          <span>
+            Question&nbsp;
+            {currentQuestion + 1}
+          </span>
+          <br></br>
+          {questions[currentQuestion].questionText}
+        </h2>
+        <ul id="btn">
+          {questions[currentQuestion].answerOptions.map((answerOption, key) => (
+            <li
+              key={key}
+              onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}
+            >
+              <Button className="mt-4" variant="primary">
+                {answerOption.answerText}
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
